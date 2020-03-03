a7afb67c8b69f788eb804499098719066de876ca
Bryan Haney <i.am.bryan.haney@gmail.com>
2020-03-03 06:48:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   data.each do |attribute, value|
-    value.each do |name|
+    attribute.each do |name|
       puts "Name: #{name}"
     end
   end
