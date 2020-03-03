869da4d1fd070c6cc3c9b814205be2a564e94f07
Bryan Haney <i.am.bryan.haney@gmail.com>
2020-03-03 06:57:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,13 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  data.each do |attribute, value|
-    value.each do |a,name|
-      puts "Name: #{name}"
+  pigeon_list = {}
+  data.each do |attribute_type, attribute_list|
+    attribute_list.each do |attribute, name_list|
+      name_list.each do |name|
+        if !pigeon_list[name] then pigeon_list[name] = {} end
+        if !pigeon_list[name][pigeon_list[name][attribute_type] = [attribute]
+        else
+          pigeon_list[name][attribute_type] << attribute]
     end
   end
 end
